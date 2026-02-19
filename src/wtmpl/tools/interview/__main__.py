38b98160b2f1c22bce98dae351c8ac1f20d000d8
anamoyee <106310501+anamoyee@users.noreply.github.com>
2026-02-19 06:11:02 +0100

    add onto the Interview idea, in the future prob move to nya-interview or whatever, for now it stays here

--- src/wtmpl/tools/interview/__main__.py
+++ src/wtmpl/tools/interview/__main__.py
@@ -3,38 +3,50 @@ from tcrutils.console import c  # type: ignore
 
 from . import Interview as I
 from . import Question__ as Q__
-from . import Transformation__ as T__
 
 if __name__ == "__main__":  # interactive tests
 	results = (
 		I(
-			name1=(
-				Q__.Label("1").  #
-				with_keep_if(lambda iv, q: 1 == 2)
-			),
+			name1=Q__.Label("1").with_keep_if(lambda iv, q: 1 == 2),
 			name2=Q__.Label("2"),
 			name3=Q__.Label("3"),
 			string=(
 				Q__
-				.Str("What is your name")
-				.with_valid_if_any_answer()
+				.Str("What is your name", default="bruh")
+				.with_valid_if_not_empty_answer()
 				.with_valid_if(
 					lambda iv, q, a: a.lower() not in ["gay", "meow"],
-					msg="[red]You must NOT say `gay` or `meow` ",
+					msg="[red]You must NOT respond with 'gay' or 'meow'!!!!!!",
 				)
 				.with_valid_if(
 					lambda iv, q, a: a.__len__() % 2 == 0,
 					msg="[red]answer len must be divisible by 2",
 				)
 			),
+			tup_subinterview=I(
+				label=Q__.Label("Enter your favorite numbers, [u]Ctrl+D to finish[/u]"),
+				tup=Q__.Tuple(
+					lambda ans_tup: Q__.Int(f"Item #{len(ans_tup) + 1}"),
+					end_condition=lambda _: False,  # never end, except for ^D, accept 0 as a valid input
+				),
+			).set_indent("| "),
 			lvl=Q__.Int("What is your lvl"),
-			default_for_next_question=Q__.YesNo("Choose next question's default", default=True),
-			start_subinterview=Q__.Dynamic(lambda iv: Q__.YesNo("Start a subinterview?", default=iv.answers["default_for_next_question"])),
-			subinterview=I(
-				name=Q__.Str("| Your name"),
-				lvl=Q__.Int("| Your lvl"),
-			).with_keep_if(lambda iv, q: iv.answers["start_subinterview"]),
+			money=Q__.Float("Enter a fair amount"),
+			default_for_next_question=Q__.YesNo("[black on white]Choose next question's default", default=True),
+			start_subinterview=Q__.Dynamic(
+				lambda iv: Q__.YesNo("Start a subinterview?", default=iv.answers["default_for_next_question"]),
+			),
+			subinterview=(
+				I(
+					label=Q__.Label("Label"),
+					name=Q__.Str("Your name"),
+					lvl=Q__.Int("Your lvl"),
+				)
+				.with_keep_if_previous_answer("start_subinterview")
+				.set_indent("| ")
+			),
 		)
+		.with_valid_if(lambda iv, q, a: False)
 		.with_rich_console(rich.console.Console(highlight=False))
 		.ask()
 	)
--- src/wtmpl/tools/interview/_base.py
+++ src/wtmpl/tools/interview/_base.py
@@ -3,7 +3,7 @@ import functools
 import os
 from collections.abc import Callable, Generator
 from contextlib import contextmanager
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from re import L
 from types import NoneType
 from typing import Any, Literal, Self, Unpack
@@ -22,6 +22,13 @@ MISSING_IN_DICT = object()
 type Textish = rich.text.TextType
 
 
+def render_textish_to_text(textish: Textish) -> rich.text.Text:
+	if isinstance(textish, str):
+		textish = rich.markup.render(textish)
+
+	return textish
+
+
 class BaseTransformation[T]:
 	type TransformFn[R] = Callable[[Interview, QuestionABC], R]
 	type ValidateFn[A] = Callable[[Interview, QuestionABC[A], A], bool]
@@ -32,8 +39,8 @@ class BaseTransformation[T]:
 	def validate(self, iv: Interview, q: QuestionABC[T], a: T) -> bool:
 		return True
 
-	def invalid_message(self, iv: Interview, q: QuestionABC[T], a: T) -> rich.text.Text | str | None:
-		return "[red]Invalid value, try again[/]"
+	def validate__invalid_message(self, iv: Interview, q: QuestionABC[T], a: T) -> rich.text.Text | None:
+		return rich.markup.render("[red]Invalid value, try again[/]")
 
 
 class Transformation__(Scope):
@@ -43,7 +50,7 @@ class Transformation__(Scope):
 
 		def transform(self, iv: Interview, q: QuestionABC) -> QuestionABC:
 			if not self.predicate(iv, q):
-				q.set_skip()
+				q.is_skipped = True
 
 			return super().transform(iv, q)
 
@@ -54,21 +61,32 @@ class Transformation__(Scope):
 			*,
 			msg: rich.text.Text | str | None = "[red]Invalid value, try again[/]",
 		) -> None:
+			msg = render_textish_to_text(msg) if msg is not None else msg
+
 			self.predicate = predicate
 			self.msg = msg
 
 		def validate(self, iv: Interview, q: QuestionABC[T], a: T) -> bool:
 			return self.predicate(iv, q, a)
 
-		def invalid_message(self, iv: Interview, q: QuestionABC[T], a: T) -> rich.prompt.Text | str | None:
+		def validate__invalid_message(self, iv: Interview, q: QuestionABC[T], a: T) -> rich.prompt.Text | None:
 			return self.msg
 
 
+@dataclass
 class QuestionABC[T](abc.ABC):
+	transformations: list[BaseTransformation] = field(init=False, default_factory=list)
+	is_skipped: bool = field(init=False, default=False)
+
+	def skip(self) -> Self:
+		"""Same as `self.is_skipped = True`, but with chaining support."""
+		self.is_skipped = True
+		return self
+
 	@abc.abstractmethod
 	def ask(self, iv: Interview, /) -> T: ...
 
-	def ask_handling_validation(self, iv: Interview, /) -> T:
+	def ask_with_validation(self, iv: Interview, /) -> T:
 		try:
 			answer = self.ask(iv)
 		except (KeyboardInterrupt, EOFError) as e:
@@ -81,14 +99,17 @@ class QuestionABC[T](abc.ABC):
 			result = trans.validate(iv, self, answer)
 
 			if not result:
-				msg = trans.invalid_message(iv, self, answer)
+				msg = trans.validate__invalid_message(iv, self, answer)
+
+				if msg is not None:
+					msg = iv.prepend_total_indent_to_text(msg)
 
 				if msg is not None:
 					iv.rich_console.print(msg)
 
 			return result  # short-circuit the all()
 
-		while not all(key(trans) for trans in self._transformations):  # 🏳️‍⚧️
+		while not all(key(trans) for trans in self.transformations):  # 🏳️‍⚧️
 			try:
 				answer = self.ask(iv)
 			except (KeyboardInterrupt, EOFError) as e:
@@ -96,30 +117,27 @@ class QuestionABC[T](abc.ABC):
 
 		return answer
 
-	def invoke_subquestion[SubT](self, subq: QuestionABC[SubT], iv: Interview) -> SubT:
-		ans = subq.ask_handling_validation(iv)
+	def invoke_subquestion[SubT](self, subq: QuestionABC[SubT], iv: Interview, *, propagate_skip: bool = True) -> SubT:
+		ans = subq.ask_with_validation(iv)
 
-		if subq._skip:
-			self.set_skip()
+		if propagate_skip and subq.is_skipped:
+			self.is_skipped = True
 
 		return ans
 
-	_skip: bool
-
-	def __init__(self) -> None:
-		self._transformations: list[BaseTransformation[T]] = []
-		self._skip = False
-
-	def set_skip(self, skip: bool = True):
-		self._skip = skip
-
 	def with_transformation(self, transformation: BaseTransformation) -> Self:
-		self._transformations.append(transformation)
+		self.transformations.append(transformation)
 
 		return self
 
 	def with_keep_if(self, predicate: BaseTransformation.TransformFn[bool]) -> Self:
 		return self.with_transformation(Transformation__.KeepIf[T](predicate))
+
+	def with_keep_if_previous_answer(self, of_question_named: str, predicate: Callable[[Any], bool] = bool) -> Self:
+		def keep_if(iv: Interview, _: QuestionABC[T], *, name=of_question_named, predicate=predicate) -> bool:
+			return predicate(iv.answers.get(name))
+
+		return self.with_keep_if(keep_if)
 
 	def with_valid_if(
 		self,
@@ -127,6 +145,8 @@ class QuestionABC[T](abc.ABC):
 		*,
 		msg: rich.text.Text | str | None = "[red]Invalid value, try again[/]",
 	) -> Self:
+		msg = render_textish_to_text(msg) if msg is not None else msg
+
 		return self.with_transformation(Transformation__.ValidIf[T](predicate, msg=msg))
 
 
@@ -142,19 +162,27 @@ class Interview(QuestionABC[dict[str, Any]]):
 			print_ctrl_d: bool = True,
 			print_ctrl_c: bool = os.name == "nt",
 		):
-			s = ""
+			self.e = e
 
-			if print_ctrl_d and isinstance(e, EOFError):
-				s += "^D"
-			if print_ctrl_c and isinstance(e, KeyboardInterrupt):
-				s += "^C"
+			extra_text = ""
+
+			if print_ctrl_d and self.originates_from_ctrl_d():
+				extra_text += "^D"
+			if print_ctrl_c and self.originates_from_ctrl_c():
+				extra_text += "^C"
 
 			if print_newline:
-				s += "\n"
+				extra_text += "\n"
 
-			print(s, end="")
+			print(extra_text, end="")
 
 			super().__init__(2)
+
+		def originates_from_ctrl_d(self) -> bool:
+			return isinstance(self.e, EOFError)
+
+		def originates_from_ctrl_c(self) -> bool:
+			return isinstance(self.e, KeyboardInterrupt)
 
 	class KeyOccupiedError(ValueError):
 		"""Raised when adding a question at a key that is already occupied."""
@@ -179,8 +207,20 @@ class Interview(QuestionABC[dict[str, Any]]):
 		self._answers: dict[str, Any] | None = None
 		self.parent_interview: Self | None = None
 		self._rich_console: rich.console.Console | None = None
+		self._indent = ""
 
-		self(**questions)
+		self.add_questions(**questions)
+
+	def set_indent(self, indent: str) -> Self:
+		self._indent = indent
+		return self
+
+	@property
+	def total_indent_text(self) -> rich.text.Text:
+		return functools.reduce(rich.text.Text.append, (render_textish_to_text(iv._indent).copy() for iv in self.parent_interviews))
+
+	def prepend_total_indent_to_text(self, content_text: rich.text.Text) -> rich.text.Text:
+		return self.total_indent_text.append(content_text)
 
 	@property
 	def parent_interviews(self) -> tuple[Interview, ...]:
@@ -195,6 +235,17 @@ class Interview(QuestionABC[dict[str, Any]]):
 		return tuple(ivs)
 
 	@property
+	def root_interview(self) -> Self:
+		"""Iteratively find the root-most `.parent_interview`."""
+
+		deepest_iv = self
+
+		while deepest_iv.parent_interview is not None:
+			deepest_iv = deepest_iv.parent_interview
+
+		return deepest_iv
+
+	@property
 	def answers(self):
 		if self._answers is None:
 			raise RuntimeError("You cannot access this helper when this Interview isn't in progress!")
@@ -206,17 +257,6 @@ class Interview(QuestionABC[dict[str, Any]]):
 		self._rich_console = rich_console
 
 		return self
-
-	@property
-	def root_iv(self) -> Self:
-		"""Iteratively find the root-most `.parent_interview`."""
-
-		deepest_iv = self
-
-		while deepest_iv.parent_interview is not None:
-			deepest_iv = deepest_iv.parent_interview
-
-		return deepest_iv
 
 	@property
 	def rich_console(self) -> rich.Console:
@@ -235,7 +275,7 @@ class Interview(QuestionABC[dict[str, Any]]):
 
 		return rich.get_console()
 
-	def __call__(self, /, **questions: QuestionABC[Any]) -> Self:
+	def add_questions(self, /, **questions: QuestionABC[Any]) -> Self:
 		"""Add a question (or multiple at a time) to this Interview, if any keys would be overwritten, raise a Interview.KeyOccupiedError. This operation is atomic."""
 		occupied_keys = {k for k in questions if k in self.questions}
 
@@ -251,15 +291,15 @@ class Interview(QuestionABC[dict[str, Any]]):
 		self._answers = {}
 
 		for name, question in self.questions.items():
-			for transformation in question._transformations.copy():
+			for transformation in question.transformations.copy():
 				question = transformation.transform(self, question)
 
-			if question._skip:
+			if question.is_skipped:
 				continue
 
-			answer = question.ask_handling_validation(self)
+			answer = question.ask_with_validation(self)
 
-			if question._skip:
+			if question.is_skipped:
 				# this check is already present higher in this loop, however this one checks if during the process of asking this flag was set, in which case simply dont include the given result.
 				continue
 
@@ -272,37 +312,71 @@ class Interview(QuestionABC[dict[str, Any]]):
 			self.parent_interview = None
 
 
+class QABCs__(Scope):
+	@dataclass(init=False)
+	class WithText[T](QuestionABC[T]):
+		_textish: Textish
+		_text: rich.text.Text | None = field(init=False, default=None)
+
+		@property
+		def text(self):
+			if self._text is None:
+				self._text = render_textish_to_text(self._textish)
+
+			return self._text
+
+	@dataclass
+	class WithDefault[T](QuestionABC[T]):
+		default: T | None = field(kw_only=True, default=None)
+		show_default: bool = field(kw_only=True, default=True)
+
+	@dataclass
+	class WithInvalidMsg[T](QuestionABC[T]):
+		invalid_msg: Textish = field(kw_only=True, default="[red]Provide a valid value")
+		_invalid_msg_text: rich.text.Text | None = field(init=False, default=None)
+
+		@property
+		def invalid_msg_text(self):
+			if self._invalid_msg_text is None:
+				self._invalid_msg_text = render_textish_to_text(self.invalid_msg)
+
+			return self._invalid_msg_text
+
+
 class Question__(Scope):
-	class Label(QuestionABC[None]):
-		def __init__(self, label: RenderableType) -> None:
-			super().__init__()
-			self.label_renderable: RenderableType = label
-
+	@dataclass
+	class Label(QABCs__.WithText[None]):
 		def ask(self, iv: Interview) -> None:
-			iv.rich_console.print(self.label_renderable)
-			self.set_skip()  # do not include in the results dict
+			iv.rich_console.print(iv.prepend_total_indent_to_text(self.text))
+			self.is_skipped = True  # do not include in the results dict
 
-	class _QuestionABCWithText[T](QuestionABC[T]):
-		def __init__(self, text: Textish) -> None:
-			super().__init__()
-
-			self.text = text
-
-	class Str(_QuestionABCWithText[str]):
+	@dataclass
+	class Str(QABCs__.WithText[str], QABCs__.WithDefault[str]):
 		def ask(self, iv: Interview) -> str:
-			return rich.prompt.Prompt.ask(self.text, console=iv.rich_console)
+			text = iv.prepend_total_indent_to_text(self.text)
 
-		def with_valid_if_any_answer(self, *, msg: rich.text.Text | str | None = "[red]Provide an answer[/]") -> Self:
+			if self.show_default and self.default is not None:
+				text.append(f" [{self.default}]")
+
+			result = rich.prompt.Prompt.ask(text, console=iv.rich_console)
+
+			if self.default is not None:
+				result = result or self.default
+
+			return result
+
+		def with_valid_if_not_empty_answer(self, *, msg: rich.text.Text | str | None = "[red]Provide a non-empty answer[/]") -> Self:
 			self.with_valid_if(lambda iv, q, a: bool(a), msg=msg)
 			return self
 
-	class Int(_QuestionABCWithText[int]):
-		def __init__(self, text: Textish, *, invalid_msg: str = "[red]Provide a valid integer") -> None:
-			super().__init__(text)
+	@dataclass
+	class Int(QABCs__.WithText[int], QABCs__.WithInvalidMsg[int]):
+		invalid_msg: Textish = field(kw_only=True, default="[red]Provide a valid integer")
 
+		def __post_init__(self) -> None:
 			self.str_question = Question__.Str(self.text).with_valid_if(
 				lambda iv, q, a: self._str_is_int_parsable(a),
-				msg=invalid_msg,
+				msg=self.invalid_msg_text,
 			)
 
 		@staticmethod
@@ -319,32 +393,47 @@ class Question__(Scope):
 
 			return int(ans)
 
-	class YesNo(_QuestionABCWithText[bool]):
-		def __init__(
-			self,
-			text: Textish,
-			default: bool | None = True,
-			show_choices: bool = True,
-		) -> None:
-			super().__init__(text)
-			self.default_str = {True: "yes", False: "no", None: None}[default]
+	@dataclass
+	class Float(QABCs__.WithText[float], QABCs__.WithInvalidMsg[float]):
+		invalid_msg: Textish = field(kw_only=True, default="[red]Provide a valid float")
 
-			if isinstance(self.text, str):
-				self.text = rich.text.Text(self.text)
+		def __post_init__(self) -> None:
+			self.str_question = Question__.Str(self.text).with_valid_if(
+				lambda iv, q, a: self._str_is_float_parsable(a),
+				msg=self.invalid_msg_text,
+			)
 
-			if show_choices:
-				parens = ("(", ")") if default is None else ("[", "]")
+		@staticmethod
+		def _str_is_float_parsable(s: str) -> bool:
+			try:
+				float(s)
+			except ValueError:
+				return False
 
-				self.text.append(
-					rich.markup.render(
-						f" {esc(parens[0])}[green b]{'Y' if default and default is not None else 'y'}[/]/[red b]{'N' if not default and default is not None else 'n'}[/]{esc(parens[1])}"
-					)
-				)
+			return True
+
+		def ask(self, iv: Interview) -> float:
+			ans = self.invoke_subquestion(self.str_question, iv)
+
+			return float(ans)
+
+	@dataclass
+	class YesNo(QABCs__.WithText[bool]):
+		default: bool | None = field(kw_only=True, default=None)
+		show_choices: bool = field(kw_only=True, default=True)
+
+		def __post_init__(self) -> None:
+			self.default_str = {True: "yes", False: "no", None: None}[self.default]
+
+			if self.show_choices:
+				parens = ("(", ")") if self.default is None else ("[", "]")
+
+				self.text.append(rich.markup.render(f"[default] {esc(parens[0])}[green b]{'Y' if self.default is True else 'y'}[/]/[red b]{'N' if self.default is False else 'n'}[/]{esc(parens[1])}"))
 
 			self.str_question = Question__.Str(self.text)
 
-			if default is None:
-				self.str_question.with_valid_if_any_answer()
+			if self.default is None:
+				self.str_question.with_valid_if_not_empty_answer()
 
 		def ask(self, iv: Interview) -> bool:
 			ans = self.invoke_subquestion(self.str_question, iv) or self.default_str
@@ -355,9 +444,51 @@ class Question__(Scope):
 			return ans.lower() in ("y", "yes")
 
 	class Dynamic[T](QuestionABC[T]):
+		"""A thin wrapper around `QuestionABC[T]`, lets you create the question at the time it's asked, not immediately. A new instance (copy) of the question will always be created if this question is asked multiple times.
+
+		```python
+		result = Interview(
+			q1=Question__.YesNo("Choose next question's default", default=True),
+			q2=Question__.Dynamic(
+				lambda iv: Question__.YesNo("Start a subinterview?", default=iv.answers["q1"]),
+			),
+		).ask()
+		```
+
+		under the hood it literally does this:
+		```python
+		def ask(self, iv: Interview) -> T:
+			return self.invoke_subquestion(self.make_question(iv), iv)
+		```
+		"""
+
 		def __init__(self, make_question: Callable[[Interview], QuestionABC[T]]) -> None:
 			super().__init__()
 			self.make_question = make_question
 
 		def ask(self, iv: Interview) -> T:
 			return self.invoke_subquestion(self.make_question(iv), iv)
+
+	@dataclass
+	class Tuple[T](QuestionABC[tuple[T, ...]]):
+		make_item_question: Callable[[tuple[T, ...]], QuestionABC[T]]
+		end_condition: Callable[[T], bool] = field(kw_only=True, default=lambda x: not x)
+		end_on_ctrl_d: bool = field(kw_only=True, default=True)
+		end_on_ctrl_c: bool = field(kw_only=True, default=False)
+
+		def ask(self, iv: Interview) -> tuple[T, ...]:
+			lst: list[T] = []
+
+			try:
+				while not self.end_condition(value := self.invoke_subquestion(self.make_item_question(tuple(lst)), iv)):
+					lst.append(value)
+			except Interview.UserExitException as e:
+				if (
+					   (self.end_on_ctrl_d and e.originates_from_ctrl_d())
+					or (self.end_on_ctrl_c and e.originates_from_ctrl_c())
+				):  # fmt: skip
+					pass
+				else:
+					raise
+
+			return tuple(lst)
